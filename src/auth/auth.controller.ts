dda9d6c873f8268ec109943fae3911e8c977f4ea
Gabriel Fagundes <gafagundesmesquita@gmail.com>
2025-06-10 16:56:13 -0300

    fix: corrigindo códigos https retornados

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,28 +1,33 @@
-import { Body, Controller, Header, Post } from '@nestjs/common';
+import { Body, Controller, Header, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginRequestDto } from './dto/request/login.request.dto';
 import { RegisterRequestDto } from 'src/auth/dto/request/register.request.dto';
+import { Code } from 'typeorm';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   async login(@Body() body: LoginRequestDto){
     return await this.authService.login(body);
   }
 
   @Post('register')
+  @HttpCode(HttpStatus.CREATED)
   async register(@Body() body: RegisterRequestDto) {
     return this.authService.register(body);
   }
 
   @Post('forgot-password')
+  @HttpCode(HttpStatus.OK)
   async forgotPassword(@Body('email') email: string){
     return this.authService.forgotPassword(email);
   }
 
   /*@Post('reset-password')
+  @HttpCode(HttpStatus.OK)
   async resetPassword(
     @Body('token') token: string,
     @Body('newPassword') newPassword: string
